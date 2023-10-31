cca09b34ba7f65b3ce6f2630a25e2d304f90e158
Shivank Pandey <shivankpandey23@gmail.com>
2023-10-31 19:53:47 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import "./App.css"
 
-function App() {
+const App = () => {
+  const[email,setEmail]=useState('')
+  const[password,setPassword]=useState('')
+  const[newData,setnewData]=useState([])
+  const dataAdded=()=>{
+    const updateddata= {id:new Date().getTime.toString(),email:email,password:password}
+    setnewData([...newData,updateddata])
+    setEmail("")
+    setPassword("")
+    console.log(updateddata)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <div className='main'>
+  <div className='sub_main'>
+<h1>Login Form</h1>
+<div className='form_data'>
+<label>Email</label> <br/>
+<input text="text" placeholder='Enter email' value={email} onChange={(e)=>{
+  setEmail(e.target.value)
+}}></input><br/>
+<label>Password</label> <br/>
+<input text="text" placeholder='Enter email'value={password} onChange={(e)=>{
+  setPassword(e.target.value)
+}}></input>
+<button className='btn' onClick={dataAdded}>Submit</button>
+</div>
+
     </div>
-  );
+    </div>
+    {
+      newData.map((curr)=>{
+        const {id,email,password}=curr;
+        return (
+          
+          <div className='data' key={id}>
+          <div className='sub_data'>
+          <h2 className='child1'> {email} </h2>
+          <h2 className='child2'>  {password}</h2>
+          </div>
+          </div>
+        )
+      })
+    }
+    </div>
+  )
 }
 
-export default App;
+export default App
